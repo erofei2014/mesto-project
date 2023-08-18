850f058d3b8422c1e8f4f1e66d900fb68ba6a726
Andrei Erofeev <erofei2014@yandex.ru>
2023-08-18 11:24:33 +0300

    feat/ поправить написание кода через prettier

--- src/components/api.js
+++ src/components/api.js
@@ -2,12 +2,12 @@
 
 //сохраняем в константу адрес и заголовок запроса
 const config = {
-  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-27',
+  baseUrl: "https://nomoreparties.co/v1/plus-cohort-27",
   headers: {
-    authorization: 'ba4ab074-000e-4a51-b901-da38460be3f2',
-    'Content-Type': 'application/json'
-  }
-}
+    authorization: "ba4ab074-000e-4a51-b901-da38460be3f2",
+    "Content-Type": "application/json",
+  },
+};
 
 //сохраняем в функцию стандартное правило при получении ответа от сервера
 function getResponseData(res) {
@@ -20,80 +20,72 @@ function getResponseData(res) {
 //запрос к серверу на получение данных профиля и аватарки
 export const getInitialUserData = () => {
   return fetch(`${config.baseUrl}/users/me`, {
-    headers: config.headers
-  })
-  .then(res => getResponseData(res));
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
 };
 
 //запрос к серверу на получение загруженных на сервер фотокарточек
 export const getInitialCards = () => {
   return fetch(`${config.baseUrl}/cards`, {
-    headers: config.headers
-  })
-  .then(res => getResponseData(res));
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
 };
 
 //запрос к серверу на обновление данных профиля
 export const patchProfileData = (usernameInput, userOccupationInput) => {
   return fetch(`${config.baseUrl}/users/me`, {
-    method: 'PATCH',
+    method: "PATCH",
     headers: config.headers,
     body: JSON.stringify({
       name: usernameInput.value,
-      about: userOccupationInput.value
-    })
-  })
-  .then(res => getResponseData(res));
+      about: userOccupationInput.value,
+    }),
+  }).then((res) => getResponseData(res));
 };
 
 //запрос к серверу на обновление аватарки
 export const patchAvatar = (userAvatarLinkInput) => {
   return fetch(`${config.baseUrl}/users/me/avatar`, {
-    method: 'PATCH',
+    method: "PATCH",
     headers: config.headers,
     body: JSON.stringify({
-      avatar: userAvatarLinkInput.value
-    })
-  })
-  .then(res => getResponseData(res));
+      avatar: userAvatarLinkInput.value,
+    }),
+  }).then((res) => getResponseData(res));
 };
 
 //запрос к серверу на добавление карточки
 export const postNewCard = (pictureTitle, pictureLink) => {
   return fetch(`${config.baseUrl}/cards`, {
-    method: 'POST',
+    method: "POST",
     headers: config.headers,
     body: JSON.stringify({
       name: pictureTitle.value,
-      link: pictureLink.value
-    })
-  })
-  .then(res => getResponseData(res));
+      link: pictureLink.value,
+    }),
+  }).then((res) => getResponseData(res));
 };
 
 //запрос к серверу на удаление карточки
 export const deleteCard = (cardId) => {
   return fetch(`${config.baseUrl}/cards/${cardId}`, {
-    method: 'DELETE',
-    headers: config.headers
-  })
-  .then(res => getResponseData(res));
+    method: "DELETE",
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
 };
 
 //запрос на установку лайка карточки
 export const addLike = (cardId) => {
   return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-    method: 'PUT',
-    headers: config.headers
-  })
-  .then(res => getResponseData(res));
+    method: "PUT",
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
 };
 
 //запрос на удаление лайка карточки
 export const removeLike = (cardId) => {
   return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-    method: 'DELETE',
-    headers: config.headers
-  })
-  .then(res => getResponseData(res));
+    method: "DELETE",
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
 };
